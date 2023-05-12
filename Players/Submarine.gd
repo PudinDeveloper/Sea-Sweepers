f1edd33e1b02fb10bf7eb1511c6a29d6bd8d1359
PudinDeveloper <dgentner@ucol.mx>
2023-05-12 00:12:45 -0600

    Fix a bug that doubles speed and submarine position in lvl2

--- Players/Submarine.gd
+++ Players/Submarine.gd
@@ -19,6 +19,21 @@ func _physics_process(delta):
 		if Input.is_action_pressed("ui_left"):
 			direction.x -= 1
 			$Sprite.flip_h = true
+		
+		if Global.singleplayer && !_antiSpeed():
+			if Input.is_action_pressed("w_up"):
+				direction.y -= 1
+			
+			if Input.is_action_pressed("s_down"):
+				direction.y += 1
+			
+			if Input.is_action_pressed("d_right"):
+				direction.x += 1
+				$Sprite.flip_h = false
+				
+			if Input.is_action_pressed("a_left"):
+				direction.x -= 1
+				$Sprite.flip_h = true			
 			
 		if Input.is_action_pressed("ui_esc"):
 			Global.trash = 0
@@ -34,3 +49,14 @@ func desaparecerSprite():
 	
 func _exitMainMenu():
 	get_tree().change_scene("res://Screens/MainMenu/main_menu.tscn")
+
+func _antiSpeed():
+	if Input.is_action_pressed("ui_up"):
+		return true
+	if Input.is_action_pressed("ui_down"):
+		return true
+	if Input.is_action_pressed("ui_right"):
+		return true
+	if Input.is_action_pressed("ui_left"):
+		return true
+	return false
